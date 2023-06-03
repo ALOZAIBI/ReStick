72abd838372f256d40f20f7e08b079067dbbdb0c
Ibrahim <ibrahim@DESKTOP-NBG2EJT.localdomain>
2023-06-04 01:28:09 +0200

    added directional movement

--- Assets/Scripts/Character/AnimationManager.cs
+++ Assets/Scripts/Character/AnimationManager.cs
@@ -16,6 +16,19 @@ public class AnimationManager : MonoBehaviour
 
     public void move(bool movementBool) {
         animator.SetBool("movementBool", movementBool);
+        Vector3 dest = agent.destination;
+        //look at unit circle
+        float angle = Mathf.Atan2(dest.y - transform.position.y, dest.x - transform.position.x)*Mathf.Rad2Deg;
+        //cuz angle goes to negatives for some reason
+        if(angle < 0) {
+            angle += 360;
+        }
+        //float angle = Vector3.Angle(transform.position, dest);
+        //if (name == "Loasp") {
+        //    Debug.Log(dest);
+        //    Debug.Log(angle);
+        //}
+        animator.SetFloat("MoveAngle",angle);
     }
 
     // Update is called once per frame
