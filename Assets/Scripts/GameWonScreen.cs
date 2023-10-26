d60c69ecf9c86355a3e01ecd061a05887586d5e7
Ibrahim <ialozaibi@gmail.com>
2023-10-27 00:02:52 +0200

    zoneWonScreen now only shows continue button that blinks to map

--- Assets/Scripts/GameWonScreen.cs
+++ Assets/Scripts/GameWonScreen.cs
@@ -17,7 +17,7 @@ public class GameWonScreen : MonoBehaviour
 
     private void Start() {
         rewardSelectHidden = rewardSelect.GetComponent<HideUI>();
-        goToNextZoneBtn.onClick.AddListener(goToNextLevelIfPossible);
+        //goToNextZoneBtn.onClick.AddListener(goToNextLevelIfPossible);
         goBackToMapBtn.onClick.AddListener(goToMap);
     }
 
@@ -53,42 +53,5 @@ public class GameWonScreen : MonoBehaviour
         //resets UI
         UIManager.singleton.pausePlayBtn.gameObject.SetActive(false);
         UIManager.singleton.timeControlHidden.hidden = true;
-    }
-
-    public void goToNextLevelIfPossible() {
-        //saves the zone
-        SaveSystem.saveZone(UIManager.singleton.zone);
-        UIManager.singleton.sceneToLoad = GetNextScene(SceneManager.GetActiveScene().name);
-        //removes characters from zone
-        foreach (Transform child in UIManager.singleton.playerParty.transform) {
-            if (child.tag == "Character") {
-                child.gameObject.SetActive(false);
-            }
-        }
-        //loads nextZone
-        UIManager.singleton.loadScene();
-        //saves characters
-        UIManager.singleton.saveMapSave();
-    }
-    //thanks chat gpt
-    public  string GetNextScene(string initZoneName) {
-        // Split the scene name into two parts: the zone and the number
-        string[] parts = initZoneName.Split('-');
-
-        // Check if the scene name is in the correct format
-        if (parts.Length != 2) {
-            throw new ArgumentException("Invalid scene name format. Expected format: ZoneX-Y");
-        }
-
-        string zone = parts[0];
-        string numberString = parts[1];
-        //Debug.Log("Zone:" + zone + "ns:" + numberString);
-        // Parse the number and increment it by 1
-        if (int.TryParse(numberString, out int number)) {
-            number++;
-            return zone + "-" + number.ToString();
-        }
-
-        throw new ArgumentException("Invalid scene name format. Number is not a valid integer.");
     }
 }
