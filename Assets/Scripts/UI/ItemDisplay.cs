2992fd7846694e5b60fc82bf62026886a9473254
Ibrahim <ialozaibi@gmail.com>
2024-06-05 11:24:34 +0200

    improved item display

--- Assets/Scripts/UI/ItemDisplay.cs
+++ Assets/Scripts/UI/ItemDisplay.cs
@@ -13,6 +13,7 @@ public class ItemDisplay : MonoBehaviour
     public TextMeshProUGUI itemName;
     public TextMeshProUGUI itemDescription;
 
+    //When adding this button is used to add item, otherwise this item displays the remove btn
     public Button wholeThingBtn;
     public Button removeBtn;
 
@@ -20,12 +21,70 @@ public class ItemDisplay : MonoBehaviour
 
     //If adding is true, clicking the display will add the item to the character
     public bool adding = false;
-    // Start is called before the first frame update
+
+    [SerializeField] private GameObject PWR;
+    [SerializeField] private TextMeshProUGUI PWRText;
+    [SerializeField] private GameObject MGC;
+    [SerializeField] private TextMeshProUGUI MGCText;
+    [SerializeField] private GameObject INF;
+    [SerializeField] private TextMeshProUGUI INFText;
+    [SerializeField] private GameObject HP;
+    [SerializeField] private TextMeshProUGUI HPText;
+    [SerializeField] private GameObject AS;
+    [SerializeField] private TextMeshProUGUI ASText;
+    [SerializeField] private GameObject CDR;
+    [SerializeField] private TextMeshProUGUI CDRText;
+    [SerializeField] private GameObject MS;
+    [SerializeField] private TextMeshProUGUI MSText;
+    [SerializeField] private GameObject Range;
+    [SerializeField] private TextMeshProUGUI RangeText;
+    [SerializeField] private GameObject LS;
+    [SerializeField] private TextMeshProUGUI LSText;
+
     void Start()
     {
         itemName.text = item.itemName;
         itemDescription.text = item.description;
         wholeThingBtn.onClick.AddListener(click);
+
+        removeBtn.onClick.AddListener(removeItem);
+        if (item.PD != 0 ){
+            PWR.SetActive(true);
+            PWRText.text = item.PD.ToString();
+        }
+        if (item.MD != 0) {
+            MGC.SetActive(true);
+            MGCText.text = item.MD.ToString();
+        }
+        if (item.INF != 0) {
+            INF.SetActive(true);
+            INFText.text = item.INF.ToString();
+        }
+        if (item.HP != 0) {
+            HP.SetActive(true);
+            HPText.text = item.HP.ToString();
+        }
+        if (item.AS != 0) {
+            AS.SetActive(true);
+            ASText.text = item.AS.ToString();
+        }
+        if (item.CDR != 0) {
+            CDR.SetActive(true);
+            CDRText.text = item.CDR.ToString();
+        }
+        if (item.MS != 0) {
+            MS.SetActive(true);
+            MSText.text = item.MS.ToString();
+        }
+        if (item.Range != 0) {
+            Range.SetActive(true);
+            RangeText.text = item.Range.ToString();
+        }
+        if (item.LS != 0) {
+            LS.SetActive(true);
+            LSText.text = item.LS.ToString();
+        }
+
     }
 
     //If we're in adding mode, add this item to the character
@@ -48,11 +107,27 @@ public class ItemDisplay : MonoBehaviour
             else
                 UIManager.singleton.saveMapSave();
         }
+        else {
+            removeBtn.gameObject.SetActive(true);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void removeItem() {
+        //If zone hasn't started
+        if (!UIManager.singleton.zoneStarted()) {
+            //Remove item from the list and place it in the item inventory
+            item.transform.parent = UIManager.singleton.playerParty.itemInventory.transform;
+            characterInfoScreen.character.items.Remove(item);
+            item.removeStats();
+
+            characterInfoScreen.displayCharacterItems(characterInfoScreen.character);
+            if (SceneManager.GetActiveScene().name == "World") {
+                UIManager.singleton.saveWorldSave();
+            }
+            else
+                UIManager.singleton.saveMapSave();
+        }
     }
+
+
 }
